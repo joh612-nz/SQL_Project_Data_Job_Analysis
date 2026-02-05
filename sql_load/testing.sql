5733661a0395f150a06cadd6229b1213066f2c1e
ohju3991 <ohju3991@student.otago.ac.nz>
2026-02-05 13:57:31 +1300

    starting sql project

--- sql_load/testing.sql
+++ sql_load/testing.sql
@@ -64,3 +64,35 @@ LEFT JOIN sk ON mar.job_id = sk.job_id
 WHERE 
     mar.salary_year_avg >70000
 ORDER BY job_posted_date;
+
+/*
+PRACTICE PROBLEM 8
+
+Find job postings from the first quarter that have a salary greater than $70k
+    - combine job posting tables from the first quarter of 2023 (Jan-Mar)
+    - gets job postings with an average yearly salary > $70,000
+*/
+
+SELECT 
+    q1_jobs.job_title_short,
+    q1_jobs.job_location,
+    q1_jobs.job_via,
+    q1_jobs.job_posted_date::DATE AS jp_date,
+    q1_jobs.salary_year_avg
+FROM (
+    SELECT *
+    FROM
+        january_2023_jobs
+    UNION ALL
+    SELECT *
+    FROM
+        february_2023_jobs
+    UNION ALL
+    SELECT *
+    FROM
+        march_2023_jobs
+) AS q1_jobs
+WHERE
+    salary_year_avg >70000
+ORDER BY
+    jp_date;
