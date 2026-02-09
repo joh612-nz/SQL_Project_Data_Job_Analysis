0fb75fefa4f6f6b01d9b17facffa587db2344447
ohju3991 <ohju3991@student.otago.ac.nz>
2026-02-10 08:57:10 +1300

    finished query 4

--- project_sql/4_top_paying_skills.sql
+++ project_sql/4_top_paying_skills.sql
@@ -6,4 +6,21 @@ Answer: what are the top skills based on salary?
     helps identify the most financially rewarding skills to acquire or improve
 */
  
- 
+SELECT
+    skills_dim.skills,
+    ROUND(AVG(salary_year_avg), 0) AS avg_salary,
+    COUNT(skills_job_dim.job_id) AS demand_count
+FROM
+    job_postings_fact
+INNER JOIN skills_job_dim ON job_postings_fact.job_id = skills_job_dim.job_id
+INNER JOIN skills_dim ON skills_job_dim.skill_id = skills_dim.skill_id
+WHERE
+    salary_year_avg IS NOT NULL
+    AND job_title_short = 'Data Analyst'
+GROUP BY
+    skills_dim.skills
+HAVING 
+    COUNT(skills_job_dim.job_id) >= 1000 
+-- only consider skills that appear in at least 1000 job postings to ensure statistical significance
+ORDER BY
+    avg_salary DESC;
