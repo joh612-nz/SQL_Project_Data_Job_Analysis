94de1ca774a3568e8efde619caf1beb7410a6a27
ohju3991 <ohju3991@student.otago.ac.nz>
2026-02-09 20:41:28 +1300

    finished query 3, working on the next one

---
+++ project_sql/4_top_paying_skills.sql
@@ -0,0 +1,9 @@
+/*
+Answer: what are the top skills based on salary?
+- look at the average salary associated with each skill for Data Analyst positions
+- focuses on roles with specified salaries, regardless of location
+- why? it reveals how different skills impact salary levels for Data Analysts and 
+    helps identify the most financially rewarding skills to acquire or improve
+*/
+ 
+ 
